21086f533cde58a0630856d1056b026b3942525a
山下真矢 <masaya@yamashitamaya-no-MacBook-Pro.local>
2019-06-02 19:39:06 +0900

    インクリメンタルサーチの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+
   def edit
   end
 
@@ -11,8 +20,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private
 
+  private
   def user_params
     params.require(:user).permit(:name, :email)
   end
